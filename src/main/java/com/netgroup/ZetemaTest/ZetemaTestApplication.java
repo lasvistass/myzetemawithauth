bf5a920a8781f9dd328905ee435cb9fecbf84123
Simone <spinelli1k90@gmail.com>
2021-11-29 12:22:39 +0100

    primo commit

---
+++ src/main/java/com/netgroup/ZetemaTest/ZetemaTestApplication.java
@@ -0,0 +1,13 @@
+package com.netgroup.ZetemaTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZetemaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZetemaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/netgroup/ZetemaTest/ZetemaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netgroup.ZetemaTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZetemaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
